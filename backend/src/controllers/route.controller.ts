bbffebb12f6ae5c7903eda65f349989483b953ab
Alison Maez <alison.c.maez@gmail.com>
2020-08-24 16:35:27 -0600

    Created a few backend files for the Routes page. Namely, a Route interface, getAllRoutes, and a route controller, which is still in progress.

---
+++ backend/src/controllers/route.controller.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from 'express';
+import {Status} from '../../utils/interfaces/Status';
+
+// Interfaces (represent the DB model and types of the columns associated with a specific DB table)
+
+import {getAllMurals} from "../../utils/mural/getAllMurals";
+
+export async function getAllMuralsController(request: Request, response: Response) {
+    try {
+        const data = await getAllMurals()
+        // return the response
+        const status: Status = {status: 200, message: null, data};
+        return response.json(status);
+    } catch(error) {
+        console.log(error);
+    }
+}
---
+++ backend/utils/interfaces/Route.ts
@@ -0,0 +1,8 @@
+export interface Route {
+    routeId: string,
+    routeContent: string,
+    routeImageUrl: string,
+    routeName: string,
+    routeNeighbourhoodLat: number,
+    routeNeighbourhoodLong: number,
+}
