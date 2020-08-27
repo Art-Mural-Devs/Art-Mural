5242b8b4800223e7c49f627e47244a3942b5f76a
ahristian <hristian.andrei@gmail.com>
2020-08-27 11:00:46 -0600

    added routes for murals

--- backend/src/routes/mural.routes.ts
+++ backend/src/routes/mural.routes.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
-import {
-    getAllMuralsController,
-    getMuralsByMostLikedController
-} from '../controllers/mural.controller';
+import {getAllMuralsController, getMuralsOrderedByMostLikedController} from '../controllers/mural.controller';
 
 
 const router = Router();
 
 // Every new route is instantiated below. It will include the controller name and the type of action (get, post, delete, put, patch)
 
-router.route("/").get(getMuralsByMostLikedController)
+router.route("/bylikes").get(getMuralsOrderedByMostLikedController)
+router.route("/:orderBy").get(getAllMuralsController)
 router.route("/").get(getAllMuralsController)
 export default router;
