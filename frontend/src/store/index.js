387251f08bdc9bdf68132aa82d72a2ce51d4c957
Joshua Duffy <Josh.duffy30@gmail.com>
2020-09-05 12:09:29 -0700

    fixed combine reducer

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,6 @@
 import {combineReducers} from "redux"
-
 import profile from "./profile";
 import murals from "./murals";
 
-export default combineReducers({murals})
-export default combineReducers({profile})
+export default combineReducers({murals,profile})
+
