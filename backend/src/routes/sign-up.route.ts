b6547375c3ed49df892bd775c9c5108fa49cb114
ahristian <hristian.andrei@gmail.com>
2020-09-08 09:34:49 -0600

    push login

--- backend/src/routes/sign-up.route.ts
+++ backend/src/routes/sign-up.route.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import { signupProfileController } from '../controllers/sign-up.controller';
-//import { signupValidator } from '../validators/signup.validator';
-//import { asyncValidatorController } from '../controllers/asyncValidator.controller';
-//import {activationController} from "../controllers/activation.controller";
+import { signUpValidator } from '../validators/signup.validator';
+import { asyncValidatorController } from '../controllers/asyncValidator.controller';
+import {activationController} from "../controllers/activation.controller";
 import {param} from "express-validator";
 
 const { checkSchema } = require('express-validator');
@@ -10,7 +10,7 @@ const { checkSchema } = require('express-validator');
 const router = Router();
 
 router.route('/')
-    //.post(asyncValidatorController(checkSchema(signupValidator)), signupProfileController);
-//router.route('/activation/:activation').get(asyncValidatorController([param("activation", "invalid activation link").isHexadecimal().notEmpty()]), activationController)
+    .post(asyncValidatorController(checkSchema(signUpValidator)), signupProfileController);
+router.route('/activation/:activation').get(asyncValidatorController([param("activation", "invalid activation link").isHexadecimal().notEmpty()]), activationController)
 
 export default router;
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(proxy('/apis', {
         logLevel: 'debug',
-        target: "http://167.172.203.59:8080",
+        target: "http://138.68.59.137:8080/",
         changeOrigin: true,
 
     }));
